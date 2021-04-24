6e2fb6c8b8b32255f15302932408247e08a6153b
wes <wesley.l.hightower@gmail.com>
2021-04-24 07:57:17 -0500

    Build pages

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 $(function () {
-  $('.carousel').carousel({ interval: 2000, pause: 'false' });
+  $('.carousel').carousel({ interval: 5000, pause: 'false' });
   $('#carouselButton').click(function() {
       if ($('#carouselButton').children('i').hasClass('fa-pause')) {
           $('.carousel').carousel('pause');
